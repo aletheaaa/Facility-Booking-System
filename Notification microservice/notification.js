375b8c4d265497af1b44379c940dc893ec73231e
Yakashimaru <114053402+Yakashimaru@users.noreply.github.com>
2023-03-16 14:51:05 +0800

    Notification

---
+++ Notification microservice/notification.js
@@ -0,0 +1,96 @@
+const amqp = require('amqplib');
+const nodemailer = require('nodemailer')
+
+const queueName = 'notification';
+const exchangeName = 'fbs';
+const routingKey = 'email.notifications'; // the routing key to use when publishing messages
+
+function mailer(details) {
+
+    var cobooker = ``
+
+    if (details.cobooker != null) {
+        cobooker = `You have indicated a cobooker when booking, ${details.cobooker} please click on the following link to accept the booking:
+https://www.google.com`
+    };
+
+    var message = `This is to inform you that your booking on ${details.date} for ${details.room} at ${details.time} has been approved. 
+${cobooker}
+Click this link for more information:
+${details.link}`
+
+    var transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          user: 'ESDFBSproj@outlook.com',
+          pass: 'ESD1235-6789FBS'
+        }
+      });
+    var mailnames = [
+        details.name,
+        details.cobooker
+    ]
+      
+    var mailOptions = {
+    from: "ESDFBSproj@outlook.com",
+    to: mailnames,
+    subject: `Facility booking for ID:${details.bookingid} confirmed`,
+    text: message
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+        console.log('Message content: '+ message)
+    }
+    });
+}
+
+async function listen() {
+  try {
+    const connection = await amqp.connect('amqp://esd-rabbit');
+    const channel = await connection.createChannel();
+
+    await channel.assertExchange(exchangeName, 'direct', { durable: true });
+    await channel.assertQueue(queueName, { durable: true });
+    await channel.bindQueue(queueName, exchangeName, routingKey);
+    console.log('Waiting for messages...');
+
+    channel.consume(queueName, (message) => {
+        if (message !== null) {
+          const content = message.content.toString();
+          try {
+            const jsonMessage = JSON.parse(content);
+            console.log(jsonMessage)
+            //console.log(`Received message: ${jsonMessage}`);
+            mailer(jsonMessage)
+          } catch (error) {
+            console.error(`Error: received non-JSON message: ${content}`);
+          }
+          channel.ack(message);
+        }
+      });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+//To create the exchange and queue if it does not exists
+async function setup() {
+  try {
+    const connection = await amqp.connect('amqp://esd-rabbit');
+    const channel = await connection.createChannel();
+
+    await channel.assertExchange(exchangeName, 'direct', { durable: true });
+    await channel.assertQueue(queueName, { durable: true });
+    await channel.bindQueue(queueName, exchangeName, routingKey);
+    console.log('Exchange and queue created.');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+setup();
+listen();
---
+++ Notification microservice/publisher_test.js
@@ -0,0 +1,31 @@
+const amqp = require('amqplib');
+
+const exchangeName = 'fbs';
+const routingKey = 'email.notifications';
+const message = {
+    "name":"joeltan.2021@scis.smu.edu.sg",
+    "bookingid":"57639218",
+    "time":"1100 - 1200",
+    "room":"SOE/SCIS2 Seminar Rm 3-3",
+    "cobooker":null,
+    "link":"https://fbs.intranet.smu.edu.sg/home",
+    "date":"15/03/23"
+};
+
+//To publish messages to the queue
+async function publishMessage() {
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    await channel.assertExchange(exchangeName, 'direct', { durable: true });
+    const messageString = JSON.stringify(message);
+    channel.publish(exchangeName, routingKey, Buffer.from(messageString));
+    console.log(`Message sent to exchange ${exchangeName} with routing key ${routingKey}`);
+    await channel.close();
+    await connection.close();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+publishMessage();
