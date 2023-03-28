8db748f0358e31a19ef5a6f5a3280b5d72823dcd
wanlint <85225600+wanlint@users.noreply.github.com>
2023-03-28 15:24:02 +0800

    Added Firebase login

---
+++ ui/src/utils/index.js
@@ -0,0 +1,57 @@
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import router from '../router/index.js';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAs7yJzR2gIxF49JzXQVkVKdw3v_DvzO-s",
+    authDomain: "wantest-9f6bf.firebaseapp.com",
+    databaseURL: "https://wantest-9f6bf-default-rtdb.firebaseio.com",
+    projectId: "wantest-9f6bf",
+    storageBucket: "wantest-9f6bf.appspot.com",
+    messagingSenderId: "76006540168",
+    appId: "1:76006540168:web:fb66edf97a4623b652b03d",
+    measurementId: "G-SDMVB049GX"
+  };
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth();
+
+export const login = () => {
+    console.log("Handling signin")
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+    }
+    // Create user with email and pass.
+    signInWithEmailAndPassword(getAuth(), email, password).then(() => {
+        console.log("Successfully signed in")
+        router.push('/main')
+    })
+
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === "auth/wrong-password") {
+                alert("Incorrect password!");
+            }
+            else if (errorCode === "auth/user-not-found") {
+                alert("User not found! Please enter a valid email address");
+            }
+            else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
+}
---
+++ ui/src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="booking-cta">
+        <h1 class="display-1 text-light font-weight-bold">SMU <br>Facilities Booking System</h1>
+    </div>
+    <div id="app">
+        <div class="login container bg-light p-5">
+            <h1 class="h1 mb-4">Login</h1>
+            <form @submit.prevent="login">
+                <div class="form-group">
+                    <p for="email">Email</p>
+                    <input
+                    class="form-control"
+                    type="text"
+                    id="email"
+                    v-model="email"
+                    placeholder="Enter your email"
+                    />
+                    
+                </div>
+                <br />
+                <div class="form-group">
+                    <p for="password">Password</p>
+                    <input
+                    class="form-control"
+                    type="password"
+                    id="password"
+                    v-model="password"
+                    placeholder="Enter your password"
+                    />
+                </div>
+                <br />
+                <button type="submit" class="btn-lg btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+        
+    </template>
+    
+    <script>
+    import { login } from '../utils'
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+        methods: {
+            login
+        },
+    }
+</script>
