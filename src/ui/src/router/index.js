5d02a3635eb1ae0f2cd8390fd5a93b90eaf50324
wanlint <85225600+wanlint@users.noreply.github.com>
2023-04-03 01:23:02 +0800

    Skeleton for Account page

--- src/ui/src/router/index.js
+++ src/ui/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import BookRoom from '../views/BookRoom.vue'
 import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
+import Account from '../views/Account.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/book',
       name: 'BookRoom',
       component: BookRoom
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: Account
     }
     
   ]
--- src/ui/src/views/Account.vue
+++ src/ui/src/views/Account.vue
@@ -35,7 +35,7 @@
 
 <script>
 export default {
-    name: 'BookRoom',
+    name: 'Account',
     data() {
         return {
 
