c1fb054489ce3b85887753423aed2284e2fe7bbb
Alethea Toh <aletheatohh@gmail.com>
2023-04-03 19:19:02 +0800

    Update payment.py

--- src/services/payment/payment.py
+++ src/services/payment/payment.py
@@ -2,11 +2,13 @@ import json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://is213@localhost:3306/accounts" 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
 db = SQLAlchemy(app)
 
